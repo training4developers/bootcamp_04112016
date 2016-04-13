7f9794abd4de2a0338a08f0b0dc4437b702e587f
Eric W. Greene <eric@ericwgreene.com>
2016-04-13 15:40:27 -0700

    added color filter

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -6,23 +6,46 @@ import { simpleSort } from './immutable';
 
 class ListOfColors extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			colorFilter: ''
+		};
+		this._textChange = this._textChange.bind(this);
+	}
+
+	_textChange(e) {
+		this.setState({
+			colorFilter: e.target.value
+		});
+	}
+
 	render() {
 
-		return <ul>
-			{simpleSort(this.props.colors).map(function(color) {
-				return <li key={color}>{color}</li>;
-			})}
-		</ul>;
+		const filteredColors = this.props.colors.filter(c =>
+				c.toLowerCase().startsWith(this.state.colorFilter.toLowerCase()));
+
+		return <div>
+			Color Filter: <input value={this.state.colorFilter} onChange={this._textChange} />
+			<ul>
+				{simpleSort(filteredColors).map(function(color) {
+					return <li key={color}>{color}</li>;
+				})}
+			</ul>
+		</div>;
 	}
 }
 
-const countryColors = ["blue", "red", "gold", "white", "green", "saffron"];
+const countryColors = [
+	'blue', 'red', 'gold', 'white', 'green', 'saffron', 'brown', 'black',
+	'gray', 'rose'
+];
 
 ReactDOM.render(<ListOfColors colors={countryColors} />,
 	document.querySelector('main'));
 
-fetch("localhost:3000/graphq/widgets?query=" + query)
-	.then(response => response.json())
-	.then(results => {
-		console.log(results.data);
-	});
+// fetch("localhost:3000/graphq/widgets?query=" + query)
+// 	.then(response => response.json())
+// 	.then(results => {
+// 		console.log(results.data);
+// 	});
